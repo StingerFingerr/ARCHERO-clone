88ba1ab128f54601a2bf20c3ba0149826f685efd
Aleksey Martinovich <martinovichmain@gmail.com>
2022-01-26 13:33:22 +0300

    Added player animator states

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using Enemy;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace DefaultNamespace
 {
     [RequireComponent(typeof(Rigidbody))]
     public class Player: MonoBehaviour
     {
+        private enum State
+        {
+            Attack,
+            Run,
+            Idle
+        }
+        
         [SerializeField] private FixedJoystick _fixedJoystick;
         [SerializeField] private float _movingSpeed;
         
@@ -14,6 +22,7 @@ namespace DefaultNamespace
         public Vector3 Position => transform.position;
 
         private Rigidbody _rb;
+        private State currentState;
         
         private void Awake()
         {
@@ -24,6 +33,7 @@ namespace DefaultNamespace
 
         private void FixedUpdate()
         {
+            
             _rb.velocity = new Vector3(_fixedJoystick.Horizontal, 0, _fixedJoystick.Vertical) * _movingSpeed;
         }
     }
