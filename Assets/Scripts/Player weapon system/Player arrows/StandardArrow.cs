60aaee1841ef9791dad60c75ca57d2c893dc73a2
Aleksey Martinovich <martinovichmain@gmail.com>
2022-01-28 21:29:23 +0300

    added shooting arrows

--- Assets/Scripts/Player weapon system/Player arrows/StandardArrow.cs
+++ Assets/Scripts/Player weapon system/Player arrows/StandardArrow.cs
@@ -1,4 +1,5 @@
-﻿using Enemy;
+﻿using System;
+using Enemy;
 using UnityEngine;
 
 namespace DefaultNamespace.Player_weapon_system
@@ -8,10 +9,16 @@ namespace DefaultNamespace.Player_weapon_system
         
         void OnTriggerEnter(Collider other)
         {
-            if (other.transform.parent.TryGetComponent(out TurretEnemy target))
+            if (other.transform.parent.TryGetComponent(out EnemyBase target))
             {
                 GiveDamage(target);
+                gameObject.SetActive(false);
             }
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            gameObject.SetActive(false);
         }
     }
 }
