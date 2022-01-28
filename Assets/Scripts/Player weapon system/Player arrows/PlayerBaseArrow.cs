d35ce870aee9e2f21f772dd948a49fe3ca27e026
Aleksey Martinovich <martinovichmain@gmail.com>
2022-01-28 20:57:47 +0300

    added standart player arrow

---
+++ Assets/Scripts/Player weapon system/Player arrows/PlayerBaseArrow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Enemy;
+using UnityEngine;
+
+namespace DefaultNamespace.Player_weapon_system
+{
+    public abstract class PlayerBaseArrow: MonoBehaviour
+    {
+        [SerializeField][Range(0,10000)] private int _damage=1;
+        [SerializeField][Range(.1f,20f)] private float _speed=1;
+
+        protected virtual void GiveDamage(IDamaged target)
+        {
+            target.SetDamage(_damage);
+        }
+    }
+}
---
+++ Assets/Scripts/Player weapon system/Player arrows/StandardArrow.cs
@@ -0,0 +1,17 @@
+﻿using Enemy;
+using UnityEngine;
+
+namespace DefaultNamespace.Player_weapon_system
+{
+    public class StandardArrow: PlayerBaseArrow
+    {
+        
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.transform.parent.TryGetComponent(out TurretEnemy target))
+            {
+                GiveDamage(target);
+            }
+        }
+    }
+}
