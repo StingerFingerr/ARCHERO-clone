64fed2661de4e7e679b928c9b8941ef01e25494a
Aleksey Martinovich <martinovichmain@gmail.com>
2022-01-28 13:52:12 +0300

    added OnEnemyKilled event

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using Enemy;
 using UnityEngine;
+using UnityEngine.Events;
 
 public abstract class EnemyBase : MonoBehaviour, ITarget, IDamaged
 {
+    public class TargetEvent : UnityEvent<ITarget> { }
+    
+    
     [SerializeField] protected EnemyHpBar _healthBar;
     [SerializeField] protected int _maxHealth;
     protected int _currentHealth;
     protected bool _isAlive;
-    
+
+    public static readonly TargetEvent OnEnemyKilled = new TargetEvent();
+
     protected abstract IEnumerator Attack();
     protected abstract IEnumerator Move();
 
@@ -19,8 +25,8 @@ public abstract class EnemyBase : MonoBehaviour, ITarget, IDamaged
         set => _isAlive = value;
     }
     public abstract bool IsVisible { get; set; }
-    
     public abstract Vector3 GetPosition();
 
     public abstract void SetDamage(int damage);
+    protected abstract void Death();
 }
