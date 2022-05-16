dd7242b3857f5fda9244983e979f4b83784ba468
Aleksey Martinovich <martinovichmain@gmail.com>
2022-05-17 01:58:57 +0300

    added faded panel

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,7 @@ using UnityEngine.Events;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private SceneBuilder _sceneBuilder;
+    [SerializeField] private UIManager _UIManager;
     [SerializeField] private Player _player;
     [SerializeField] private Vector3 _playerStartPos;
 
@@ -28,10 +29,12 @@ public class GameManager : MonoBehaviour
 
     
 
-    public void StartGame()
+    public async void StartGame()
     {
         _level = 0;
         OpenNextLevel();
+        await Task.Delay(900);
+        _UIManager.CloseMenuPanel();
     }
 
     private void DecrementEnemiesCount(ITarget targer)
@@ -45,9 +48,12 @@ public class GameManager : MonoBehaviour
 
     private async void OpenNextLevel()
     {
-        if(_level > 1)
+        _UIManager.OpenFadedPanel();
+        await Task.Delay(1000);
+        _UIManager.CloseFadedPanel();
+        if (_level > 1)
         {
-            await Task.Delay(3000);
+            
         }
         _level++;
         _enemiesOnScene = _sceneBuilder.CreateLevel(_level);
